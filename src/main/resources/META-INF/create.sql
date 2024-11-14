2ce60c1fbdfe5aa843bd815fb2496bd1a4f2f2c5
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-11-14 16:56:18 +0700

    Update databaseName (for each class)

--- src/main/resources/META-INF/create.sql
+++ src/main/resources/META-INF/create.sql
@@ -1,9 +1,9 @@
-drop table if exists products10;
-create table products10 (id bigint not null, name varchar(255), primary key (id));
-INSERT INTO products10 (id, name) VALUES (1, 'Product A');
-INSERT INTO products10 (id, name) VALUES (2, 'Product B');
-INSERT INTO products10 (id, name) VALUES (3, 'Product C');
-INSERT INTO products10 (id, name) VALUES (4, 'Product D');
+-- drop table if exists products10;
+-- create table products10 (id bigint not null, name varchar(255), primary key (id));
+-- INSERT INTO products10 (id, name) VALUES (1, 'Product A');
+-- INSERT INTO products10 (id, name) VALUES (2, 'Product B');
+-- INSERT INTO products10 (id, name) VALUES (3, 'Product C');
+-- INSERT INTO products10 (id, name) VALUES (4, 'Product D');
 
 drop table if exists students;
 create table students (id bigint not null, name varchar(50), email varchar(50), phone varchar(15), primary key (id));
